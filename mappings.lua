7b963e1aef42af5acf47ddd0204fcc1e7542b442
Guillaume Bodi <bodi.giyomu@gmail.com>
2022-10-30 11:06:29 +0900

    Update config

--- mappings.lua
+++ mappings.lua
@@ -2,14 +2,11 @@ return {
   n = {
     ["<leader>d"] = false,
     ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>H"] = { "<cmd>Alpha<cr>", desc = "Alpha Dashboard" },
     -- Debugger
     ["<F5>"] = { "<cmd>lua require'dap'.continue()<cr>", desc = "Debugger: Start" },
     ["<F6>"] = { "<cmd>lua require'dap'.terminate()<cr>", desc = "Debugger: Terminate" },
     ["<F10>"] = { "<cmd>lua require'dap'.step_over()<cr>", desc = "Debugger: Step over" },
     ["<F11>"] = { "<cmd>lua require'dap'.step_into()<cr>", desc = "Debugger: Step into" },
     ["<F12>"] = { "<cmd>lua require'dap'.step_out()<cr>", desc = "Debugger: Step out" },
-  },
-  t = {
   },
 }
